c7c08c845ff86eebddb1fa00a812c6f7d14e9cfa
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2023-04-21 00:42:18 -0700

    Speed up to search using better multi-threading

--- multithreaded.cpp
+++ multithreaded.cpp
@@ -7,14 +7,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-#include <sys/stat.h>
 #include <git2.h>
 #include <hs/hs.h>
+#include <algorithm>
+#include <atomic>
+#include <sstream>
+#include <sys/stat.h>
+
+#define START   auto start = std::chrono::high_resolution_clock::now();
+#define STOP   auto end = std::chrono::high_resolution_clock::now(); \
+  auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count(); \
+  if (diff > 0) { \
+    std::cout << diff << "\n"; \
+  }
 
 git_repository* repo = nullptr;
 hs_database_t *database = NULL;
 hs_scratch_t *scratch = NULL;
 
+// 200-400us
 bool is_ignored(const char* path)
 {
     if (!repo) {
@@ -66,59 +77,99 @@ static int on_match(unsigned int id, unsigned long long from,
     {
         // New file
         // Print filename
-        std::lock_guard<std::mutex> lock(cout_mutex);
+        // std::lock_guard<std::mutex> lock(cout_mutex);
         std::cout << filename << "\n";
         current_file_num = file_num;
     }
 
     if (end > start) {
-        // std::string_view line(&data[start], end - start);
+        std::string_view line(&data[start], end - start);
         // std::lock_guard<std::mutex> lock(cout_mutex);
-        // std::cout << line << "\n";
+        std::cout << line << "\n";
     }
+
     return 0;
 }
 
-void process_chunk(const std::string& filename, int chunk_num, const std::streampos file_size) {
-  std::ifstream file(filename, std::ios::binary);
+// void process_file(std::string_view filename, const std::streampos file_size) {
+
+//   std::ifstream file(filename.data(), std::ios::binary);
+//   if (!file) {
+//     std::cerr << "Failed to open file: " << filename << std::endl;
+//     return;
+//   }
+
+//   const std::streampos start_pos = 0;
+//   const std::streampos end_pos = file_size;
+
+//   file.seekg(start_pos);
+//   const std::size_t chunk_size = static_cast<std::size_t>(end_pos - start_pos);
+//   std::vector<char> chunk_data(chunk_size);
+
+//   file.read(chunk_data.data(), chunk_size);
+
+//   // Check for any NULL byte in chunk
+//   // const char nullbyte = '\0';  
+//   // if (std::find(chunk_data.begin(), chunk_data.end(), nullbyte) == chunk_data.end()) {
+//     // Process the chunk here
+//     file_context ctx { filename.data(), chunk_data.data(), chunk_data.size() };
+//     hs_scan(database, chunk_data.data(), chunk_data.size(), 0, scratch, on_match, (void *)(&ctx));
+//   // }
+
+// }
+
+void process_file(std::string_view filename, const std::streampos file_size) {
+  std::ifstream file(filename.data(), std::ios::binary);
   if (!file) {
     std::cerr << "Failed to open file: " << filename << std::endl;
     return;
   }
 
-  const std::streampos start_pos = chunk_num * CHUNK_SIZE;
-  const std::streampos end_pos = std::min(start_pos + CHUNK_SIZE, file_size);
+  const std::size_t first_chunk_size = 256; // read first 256 bytes to check if binary
+  const std::size_t chunk_size = 1024 * 1024; // read in 1 MB chunks
+  char* chunk_data = new char[chunk_size];
 
-  file.seekg(start_pos);
-  const std::size_t chunk_size = static_cast<std::size_t>(end_pos - start_pos);
-  std::vector<char> chunk_data(chunk_size);
+  // Read first chunk to check if binary
+  file.read(chunk_data, first_chunk_size);
+  bool is_binary = std::find(chunk_data, chunk_data + first_chunk_size, '\0') != chunk_data + first_chunk_size;
 
-  file.read(chunk_data.data(), chunk_size);
-  if (file.gcount() != chunk_size) {
-    std::cerr << "Failed to read chunk " << chunk_num << " from file: " << filename << std::endl;
+  if (is_binary) {
+    delete[] chunk_data;
     return;
   }
 
-  // Process the chunk here
-  file_context ctx { filename.data(), chunk_data.data(), chunk_data.size() };
-  hs_scan(database, chunk_data.data(), chunk_data.size(), 0, scratch, on_match, (void *)(&ctx));
+  std::streampos bytes_read = first_chunk_size;
+  while (bytes_read < file_size) {
+    const std::streampos remaining_bytes = file_size - bytes_read;
+    const std::size_t bytes_to_read = static_cast<std::size_t>(std::min<std::streampos>(remaining_bytes, chunk_size));
 
-  // Print the chunk number
-  // std::lock_guard<std::mutex> lock(cout_mutex);
-  // std::cout << "Processed chunk " << chunk_num << " (" << start_pos << "," << end_pos << ")" << std::endl;
+    file.read(chunk_data, bytes_to_read);
+
+    // Process the chunk here
+    file_context ctx { filename.data(), chunk_data, bytes_to_read };
+    hs_scan(database, chunk_data, bytes_to_read, 0, scratch, on_match, (void *)(&ctx));
+
+    bytes_read += bytes_to_read;
+  }
+
+  delete[] chunk_data;
 }
 
-int visit(const char *path) {
+std::size_t n{0};
+
+static inline int visit(const char *path) {
     DIR *dir = opendir(path);
     if (dir == NULL) {
         // perror("opendir");
         return -1;
     }
 
+    std::vector<std::thread> visit_threads;
+
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
 
-        // Construct path
+        // Construct path // 8-10us
         const size_t path_len = strlen(path);
         const size_t name_len = _D_EXACT_NAMLEN(entry);
         const size_t total_len = path_len + 1 + name_len + 1;
@@ -128,49 +179,45 @@ int visit(const char *path) {
         memcpy(filepath + path_len + 1, entry->d_name, name_len);
         filepath[total_len - 1] = '\0';
 
+        // Process the file in chunks, multithreaded
+        std::string_view filename = filepath;
+
         // Check if path is a directory
         if (entry->d_type == DT_DIR) {
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
                 continue;
             }
+            // std::cout << filename << " is a DIR\n";
             if (!is_ignored(filepath))
             {
-                if (visit(filepath) == -1) {
-                    closedir(dir);
-                    return -1;
-                }
+              visit_threads.push_back(std::thread([filename = std::string(filepath)]() {
+                // std::cout << filename << "\n";
+                visit(filename.data());
+              }));
             }
         }
         // Check if path is a regular file 
         else if (entry->d_type == DT_REG) {
 
-            if (!is_ignored(filepath))
-            {
-                std::string filename = filepath;
-                file_num += 1;
+            // if (!is_ignored(filepath))
+            // {
+              file_num += 1;
 
-                std::ifstream file(filename, std::ios::binary);
-                if (!file) {
-                  std::cerr << "Failed to open file: " << filename << std::endl;
+              struct stat fileStat;
+              if (stat(filepath, &fileStat) == -1) {
+                  perror("Error");
                   return 1;
-                }
+              }
+              const std::streampos file_size = fileStat.st_size;
+              process_file(filename, file_size);
 
-                file.seekg(0, std::ios::end);
-                const std::streampos file_size = file.tellg();
-                file.seekg(0, std::ios::beg);
-
-                const int num_chunks = static_cast<int>(((int)(file_size + CHUNK_SIZE) - 1) / CHUNK_SIZE);
-                std::vector<std::thread> threads(num_chunks);
-
-                for (int i = 0; i < num_chunks; ++i) {
-                  threads[i] = std::thread(process_chunk, filename, i, file_size);
-                }
-
-                for (auto& thread : threads) {
-                  thread.join();
-                }
-            }
+              n += 1;
+            // }
         }
+    }
+
+    for (auto& t : visit_threads) {
+      t.join();
     }
 
     closedir(dir);
@@ -178,6 +225,8 @@ int visit(const char *path) {
 }
 
 int main(int argc, char** argv) {
+  std::ios_base::sync_with_stdio(false);
+
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
     return 1;
@@ -187,7 +236,11 @@ int main(int argc, char** argv) {
   const char *pattern = argv[2];
 
   char resolved_path[PATH_MAX]; 
-  realpath(path, resolved_path); 
+  char* ret = realpath(path, resolved_path); 
+  if (ret == NULL) {
+    std::cerr << "Error with realpath\n";
+    return 1;
+  }
 
   git_libgit2_init();
   if (git_repository_open(&repo, resolved_path) < 0) {
@@ -217,6 +270,8 @@ int main(int argc, char** argv) {
   }
 
   git_repository_free(repo);
+
+  std::cout << "\nSearched " << n << " files\n";
 
   return 0;
 }
