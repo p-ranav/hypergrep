ac7e2e5c7d57e0a6c244fb12ef5c6883cb21e871
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2023-05-09 10:17:37 -0500

    Removed unnecessary path and string allocation

--- src/directory_search.cpp
+++ src/directory_search.cpp
@@ -347,9 +347,8 @@ bool directory_search::visit_git_index(const std::filesystem::path &dir,
     while (git_index_iterator_next(&entry, iter) != GIT_ITEROVER) {
       if (entry && (!options.filter_files ||
                     (options.filter_files && filter_file(entry->path)))) {
-        const auto path = dir / entry->path;
-        queue.enqueue(ptok, path.string());
-        ++num_files_enqueued;         
+        queue.enqueue(ptok, entry->path);
+        ++num_files_enqueued;          
       }
     }
     git_index_iterator_free(iter);
