7f9bccdb3ea87fb81c4cfe4d5b1fbe11eb59d23c
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2023-05-17 12:01:35 -0700

    Started implementation for improved file search

--- src/file_search.cpp
+++ src/file_search.cpp
@@ -85,6 +85,216 @@ void file_search::run(std::filesystem::path path) {
   mmap_and_scan(std::move(path));
 }
 
+struct chunk_result {
+  char* start{nullptr};
+  char* end{nullptr};
+  std::vector<std::pair<unsigned long long, unsigned long long>> matches{};
+};
+
+bool file_search::mmap_and_scan(std::string &&filename) {
+  int fd = open(filename.data(), O_RDONLY, 0);
+  if (fd == -1) {
+    return false;
+  }
+
+  // Get the size of the file
+  struct stat sb;
+  if (fstat(fd, &sb) == -1) {
+    return false;
+  }
+  std::size_t file_size = sb.st_size;
+
+  // Memory map the file
+  char *buffer = (char *)mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (buffer == MAP_FAILED) {
+    return false;
+  }
+
+  // Use the data
+
+  // NOTE: hs_scan takes a `unsigned int` buffer size (2^32-1)
+  // If the file_size is larger than that (e.g., 13GB), then
+  // the search would need to be performed in chunks
+  std::size_t max_searchable_size =
+      FILE_CHUNK_SIZE; // std::numeric_limits<unsigned int>::max() - 1; // max
+                       // that hs_scan can handle
+
+  // Algorithm:
+  // Spawn N-1 threads (N = max hardware concurrency)
+  // Each thread will be given: buffer, file_size, offset
+  // Each thread will search from the bytes {buffer + offset, buffer + offset +
+  // search_size} Each thread will print its results
+
+  auto max_concurrency = options.num_threads;
+  if (max_concurrency > 1) {
+    max_concurrency -= 1;
+  }
+
+  // Each thread will enqueue its results into its queue
+  moodycamel::ConcurrentQueue<chunk_result>
+      *output_queues =
+          new moodycamel::ConcurrentQueue<chunk_result>[max_concurrency];
+
+  std::vector<std::thread> threads(max_concurrency);
+  thread_local_scratch.reserve(max_concurrency);
+
+  std::atomic<std::size_t> num_threads_finished{0};
+  std::atomic<std::size_t> num_results_enqueued{0}, num_results_dequeued{0};
+  std::atomic<std::size_t> num_matches{0};
+
+  for (std::size_t i = 0; i < max_concurrency; ++i) {
+
+    // Set up the scratch space
+    hs_scratch_t *local_scratch = NULL;
+    hs_error_t database_error = hs_alloc_scratch(database, &local_scratch);
+    if (database_error != HS_SUCCESS) {
+      fprintf(stderr, "Error allocating scratch space\n");
+      hs_free_database(database);
+      return false;
+    }
+    thread_local_scratch.push_back(local_scratch);
+
+    // Spawn a reader thread
+    threads[i] = std::thread([this, i = i, max_concurrency = max_concurrency,
+                              buffer = buffer, file_size = file_size,
+                              max_searchable_size = max_searchable_size,
+                              &output_queues, &num_matches,
+                              &num_results_enqueued,
+                              &num_threads_finished]() {
+
+      // Set up the scratch space
+      hs_scratch_t *local_scratch = thread_local_scratch[i];
+
+      std::size_t offset{i * max_searchable_size};
+      char *eof = buffer + file_size;
+
+      while (true) {
+
+        char *start = buffer + offset;
+        if (start > buffer + file_size) {
+          // stop here
+          num_threads_finished += 1;
+          break;
+        }
+
+        char *end = start + max_searchable_size;
+        if (end > buffer + file_size) {
+          end = buffer + file_size;
+        }
+
+        if (offset > 0) {
+          // Adjust start to go back to a newline boundary
+          // Adjust end as well to go back to a newline boundary
+          auto previous_start = start - max_searchable_size;
+          if (previous_start > buffer) {
+            std::string_view chunk(previous_start, max_searchable_size);
+
+            auto last_newline = chunk.find_last_of('\n', max_searchable_size);
+            if (last_newline == std::string_view::npos) {
+              // No newline found, do nothing?
+              // TODO: This could be an error scenario, check
+            } else {
+              start = previous_start + last_newline;
+            }
+          } else {
+            // Something wrong, don't update start
+          }
+        }
+
+        // Update end to stop at a newline boundary
+        std::string_view chunk(start, end - start);
+        auto last_newline = chunk.find_last_of('\n', end - start);
+        if (last_newline == std::string_view::npos) {
+          // No newline found, do nothing?
+          // TODO: This could be an error scenario, check
+        } else {
+          end = start + last_newline;
+          if (offset == 0) {
+            end += 1;
+          }
+        }
+
+        if (static_cast<std::size_t>(eof - end) < max_searchable_size) {
+          // If "end" and EOF are separated by less than
+          // a single chunk
+          // just set end to the EOF
+          end = eof;
+        }
+
+        // Perform the search
+        std::mutex match_mutex;
+        std::vector<std::pair<unsigned long long, unsigned long long>>
+            matches{};
+        std::atomic<size_t> number_of_matches = 0;
+        file_context
+	      ctx{number_of_matches, matches, match_mutex, false /* print_only_filenames is not relevant in a single file search */};
+
+        if (hs_scan(database, start, end - start, 0, local_scratch, on_match,
+                    (void *)(&ctx)) != HS_SUCCESS) {
+          num_threads_finished += 1;
+          break;
+        }
+
+        if (ctx.number_of_matches > 0) {
+          num_matches += ctx.number_of_matches;
+          // Save result
+          chunk_result local_chunk_result { start, end, std::move(matches) };
+          output_queues[i].enqueue(std::move(local_chunk_result));
+          num_results_enqueued += 1;
+        }
+
+        offset += max_concurrency * max_searchable_size;
+      }
+
+    });
+
+  }
+
+  // In this main thread
+  // Dequeue from output_queues, process matches
+  // and print output
+  std::size_t i = 0;
+  while (!(num_threads_finished == max_concurrency && num_results_enqueued == num_results_dequeued)) {
+    chunk_result next_result{};
+
+    auto found = output_queues[i].try_dequeue(next_result);
+    if (found) {
+
+      // Do something with result
+      // Print it
+
+      num_results_dequeued += 1;
+      fmt::print("{} {}/{} {}/{}\n", i, num_threads_finished, max_concurrency, num_results_dequeued, num_results_enqueued);
+
+      i += 1;
+      if (i == max_concurrency) {
+        i = 0;
+      }
+    }
+  }
+
+  for (auto &t : threads) {
+    t.join();
+  }
+
+  fmt::print("{}\n", num_matches);
+
+  // Unmap the file
+  if (munmap(buffer, file_size) == -1) {
+    return false;
+  }
+
+  // Close the file
+  if (close(fd) == -1) {
+    return false;
+  } 
+
+  delete[] output_queues;
+
+  return true;
+}
+
+#if 0
 bool file_search::mmap_and_scan(std::string &&filename) {
 
   int fd = open(filename.data(), O_RDONLY, 0);
@@ -317,12 +527,13 @@ bool file_search::mmap_and_scan(std::string &&filename) {
   // Close the file
   if (close(fd) == -1) {
     return false;
-  }
+  } 
 
   delete[] inter_thread_synchronization_line_count_queue;
 
   return true;
 }
+#endif
 
 bool file_search::scan_line(std::string &line,
                             std::size_t &current_line_number) {
