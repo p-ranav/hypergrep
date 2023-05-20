a422652a32d10ef46f913e962a6d3a1c35ecab4f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2023-05-20 12:59:30 -0500

    Not printing a newline before first filename

--- src/directory_search.cpp
+++ src/directory_search.cpp
@@ -169,6 +169,7 @@ void directory_search::compile_hs_database(std::string &pattern) {
 bool directory_search::process_file(std::string &&filename,
                                     hs_scratch_t *local_scratch, char *buffer,
                                     std::string &lines) {
+  static bool first_file{true};
   int fd = open(filename.data(), O_RDONLY, 0);
   if (fd == -1) {
     return false;
@@ -309,8 +310,13 @@ bool directory_search::process_file(std::string &&filename,
     } else if (result && !lines.empty()) {
       if (options.is_stdout) {
         if (options.print_filenames) {
+          if (!first_file) {
+            lines += "\n";
+          } else {
+            first_file = false;
+          }
           lines =
-              fmt::format(fg(fmt::color::steel_blue), "\n{}\n", filename) + lines;
+              fmt::format(fg(fmt::color::steel_blue), "{}\n", filename) + lines;
         } else {
           lines += "\n";
         }
