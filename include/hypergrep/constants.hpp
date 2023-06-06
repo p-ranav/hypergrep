f5e55ca6e6f4f1c49a10f04b319b1359642097f6
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2023-06-05 21:30:06 -0500

    Bumped to version 0.1.1

--- include/hypergrep/constants.hpp
+++ include/hypergrep/constants.hpp
@@ -4,7 +4,7 @@
 constexpr static inline std::string_view NAME = "hg";
 constexpr static inline std::string_view DESCRIPTION =
     "Recursively search directories for a regex pattern";
-constexpr static inline std::string_view VERSION = "0.1.0";
+constexpr static inline std::string_view VERSION = "0.1.1";
 constexpr static inline std::size_t TYPICAL_FILESYSTEM_BLOCK_SIZE = 4096;
 constexpr static inline std::size_t FILE_CHUNK_SIZE =
     16 * TYPICAL_FILESYSTEM_BLOCK_SIZE;
